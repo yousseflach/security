0730bc3ad7b7e71de6e8c91a64d843c588ad8ad0
Lachgar Youssef <lachgar.youssef0@gmail.com>
2024-05-07 23:29:40 +0100

    setuping AuthenticationController

--- src/main/java/com/lachgar/security/Application.java
+++ src/main/java/com/lachgar/security/Application.java
@@ -1,13 +1,30 @@
 package com.lachgar.security;
 
+import com.lachgar.security.entity.RoleEntity;
+import com.lachgar.security.repository.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner runner(RoleRepository roleRepository){
+		return  args -> {
+			if (roleRepository.findByName("USER").isEmpty()){
+				roleRepository.save(
+						RoleEntity.builder().name("USER").build()
+				);
+			}
+		};
 	}
 
 }
--- src/main/java/com/lachgar/security/controller/AuthenticationController.java
+++ src/main/java/com/lachgar/security/controller/AuthenticationController.java
@@ -1,4 +1,36 @@
 package com.lachgar.security.controller;
 
+import com.lachgar.security.dto.AuthenticationRequestDTO;
+import com.lachgar.security.dto.JwtResponseDTO;
+import com.lachgar.security.dto.RegistrationRequestDTO;
+import com.lachgar.security.service.AuthenticationService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.mail.MessagingException;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("auth")
+@RequiredArgsConstructor
+@Tag(name = "Authentication")
 public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public ResponseEntity<?> register(@RequestBody @Valid RegistrationRequestDTO request) throws MessagingException {
+        authenticationService.register(request);
+        return ResponseEntity.accepted().build();
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<JwtResponseDTO> authenticate(
+            @RequestBody @Valid AuthenticationRequestDTO request
+    ){
+        return ResponseEntity.ok(authenticationService.authenticate(request));
+    }
 }
